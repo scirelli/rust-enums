aa74bacba81e44aafb424068367ffba5fc1135ce
Steve Cirelli <scirelli+git@gmail.com>
2025-07-28 21:09:53 -0400

    quick save

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#![allow(unused)]
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call_me(self) {
+        match self {
+            Message::Quit => println!("AHHH! Bye"),
+            Message::Move {x, y} => println!("{x} {y}"),
+            Message::Write(s) => println!("{s}"),
+            Message::ChangeColor(r, g, b) => println!("{r} {g} {b}"),
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let w = Message::Write(String::from("Hello"));
+    w.call_me();
+    w.call_me();
 }
